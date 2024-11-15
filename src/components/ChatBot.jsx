2cba766886e22c70df80a162b6b5ac033e89878d
Doyeon <duseh729@naver.com>
2024-11-15 19:39:56 +0900

    feat: elice helpy api 연동

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,3 +1,33 @@
-const ChatBot = ()=>{}
+import { useState } from "react";
+import postChat from "../api/eliceChat";
+
+const ChatBot = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  return (
+    <>
+      챗임
+      <br />
+      <input
+        type="text"
+        onChange={e => {
+          setText(e.target.value);
+          // console.log(text);
+        }}
+      />
+      <button
+        onClick={async e => {
+          const response = await postChat(text);
+          // console.log(response.data)
+          setResult(response.data?.choices[0]?.message?.content)
+        }}
+      >보내기</button>
+      <div>
+        <span>{result}</span>
+      </div>
+    </>
+  );
+};
 
 export default ChatBot;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
+import ChatBot from "../components/ChatBot";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="chat" element={<ChatBot/>} />
       </Routes>
     </BrowserRouter>
   );
